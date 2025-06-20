79dff2a2595befe4d83e6497deb52ec01c03b05e
Aleksandra Plavskaya <a.ichetkina@gmail.com>
2025-06-20 20:02:39 +0300

    first commit

---
+++ src/main/java/ru/netology/Candidate.java
@@ -0,0 +1,57 @@
+package ru.netology;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Candidate {
+
+    private String id;//уникальный идентификатор записи
+    private String text; // сообщение
+    private String type;// тип животного
+    private String user; // имя пользователя
+    private String upvotes; // голоса
+
+    public Candidate(
+            @JsonProperty("id") String id,
+            @JsonProperty("text") String text,
+            @JsonProperty("type") String type,
+            @JsonProperty("user") String user,
+            @JsonProperty("upvotes") String upvotes
+    ) {
+        this.id = id;
+        this.text = text;
+        this.type = type;
+        this.user = user;
+        this.upvotes = upvotes;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getUpvotes() {
+        return upvotes;
+    }
+
+    @Override
+    public String toString() {
+        return "Candidate{" +
+                "id='" + id + '\'' +
+                ", text='" + text + '\'' +
+                ", type='" + type + '\'' +
+                ", user='" + user + '\'' +
+                ", upvotes='" + upvotes + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/ru/netology/Main.java
@@ -0,0 +1,37 @@
+package ru.netology;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.util.List;
+
+
+public class Main {
+    public static final String URL = "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+    public static ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) throws Exception {
+
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)    // максимальное время ожидание подключения к серверу
+                        .setSocketTimeout(30000)    // максимальное время ожидания получения данных
+                        .setRedirectsEnabled(false) // возможность следовать редиректу в ответе
+                        .build())
+                .build();
+        HttpGet request = new HttpGet(URL);
+        CloseableHttpResponse response = httpClient.execute(request);
+        List<Candidate> candidates = mapper.readValue(
+                response.getEntity().getContent(), new
+                        TypeReference<>() {
+                        });
+        List<Candidate> result = candidates.stream().filter(value -> value.getUpvotes() != null).toList();
+        result.forEach(System.out::println);
+
+    }
+}
